90961602f5cf9de278131b9f5a4e4515abf749ae
ar729 <ar729@cornell.edu>
2022-05-15 13:54:09 -0400

    push correct blender/glb scenes

--- Final/rename_bones.py
+++ Final/rename_bones.py
@@ -1,6 +1,6 @@
 from pygltflib import GLTF2
-INPUT_FILE = "../resources/scenes/final_test.glb"
-OUTPUT_FILE = "../resources/scenes/final_final.glb"
+INPUT_FILE = "../resources/scenes/final_30b_rtm.glb"
+OUTPUT_FILE = "../resources/scenes/final_30b_rtm_edited_bones.glb"
 gltf = GLTF2.load(INPUT_FILE)
 count = 1
 for node in gltf.nodes:
